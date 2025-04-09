ca343452f06a3108afd640d8da93ac5964ddfe73
dbqls61 <dbqls61_@naver.com>
2025-04-09 22:17:12 +0900

    Title: 기수 정렬 구현, Time: 59ms, Memory: 2MB, Status: Passed - Codetree

--- 250409/기수 정렬 구현/implement-radix-sort.cpp
+++ 250409/기수 정렬 구현/implement-radix-sort.cpp
@@ -3,44 +3,35 @@
 
 using namespace std;
 
-int getDigit(int number, int pos) {
-    int div = 1;
-    while (pos--) div *= 10;
-    return (number / div) % 10;
-}
+int n;
+int arr[100000];
 
-void radix_sort(int arr[], int n, int k) {
-    for (int pos = 0; pos < k; pos++) {
-        vector<vector<int>> arr_new(10);
-
-        for (int i = 0; i < n; i++) {
-            int digit = getDigit(arr[i], pos);
-            arr_new[digit].push_back(arr[i]);
-        }
-
-        int idx = 0;
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < arr_new[i].size(); j++) {
-                arr[idx++] = arr_new[i][j];
-            }
+void radix_sort(int arr[], int exp) {
+    vector<int> new_arr;
+    if (exp > 100000) return;
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < n; j++) {
+            if (arr[j] / exp % 10 == i)
+                new_arr.push_back(arr[j]);
         }
     }
+    for (int i = 0; i < n; i++) {
+        arr[i] = new_arr[i];
+    }
+    radix_sort(arr, exp * 10);
 }
 
 int main() {
-    int n;
-    int arr[100000];
-
     cin >> n;
+
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
     }
 
-    radix_sort(arr, n, 1);
+    radix_sort(arr, 1);
 
     for (int i = 0; i < n; i++) {
-        cout << arr[i] << ' ';
+        cout << arr[i] << " ";
     }
-
     return 0;
 }
