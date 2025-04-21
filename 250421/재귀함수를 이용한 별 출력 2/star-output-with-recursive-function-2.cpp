87033213af736b9c60a9b9acf745f3bd04e24141
dbqls61 <dbqls61_@naver.com>
2025-04-21 17:14:34 +0900

    Title: 재귀함수를 이용한 별 출력 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250421/재귀함수를 이용한 별 출력 2/star-output-with-recursive-function-2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+
+void star (int n) {
+    if (n==0)
+        return;
+
+    for (int i=0; i<n; i++)
+        cout << "* ";
+    cout << endl;
+    star(n-1);
+    for (int i=0; i<n; i++)
+        cout << "* ";
+    cout << endl;
+}
+
+int main() {
+    cin >> n;
+
+    star(n);
+
+    return 0;
+}
