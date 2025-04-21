38e2721d5db2bab1a7c1da0478d3213d8a448c22
dbqls61 <dbqls61_@naver.com>
2025-04-21 21:14:34 +0900

    Title: 정수 명령 처리 5, Time: 10ms, Memory: 1MB, Status: Passed - Codetree

---
+++ 250421/정수 명령 처리 5/process-numeric-commands-5.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int N;
+string command[10000];
+int num[10000];
+
+int main() {
+    vector<int> v;
+    cin >> N;
+
+    for (int i = 0; i < N; i++) {
+        cin >> command[i];
+        if (command[i] == "push_back" || command[i] == "get") {
+            cin >> num[i];
+        }
+    }
+    for (int i = 0; i < N; i++) {
+        if (command[i] == "push_back") {
+            v.push_back(num[i]);
+        }
+        if (command[i] == "pop_back") {
+            v.pop_back();
+        }
+        if (command[i] == "size") {
+            cout << v.size() << endl;
+        }
+        if (command[i] == "get") {
+            cout << v[num[i]-1] << endl;
+        }
+    }
+
+    return 0;
+}
