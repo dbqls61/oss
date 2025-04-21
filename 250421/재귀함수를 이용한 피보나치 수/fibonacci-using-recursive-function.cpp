c054728de423fa0c7a2069279cab979ba39de7ef
dbqls61 <dbqls61_@naver.com>
2025-04-21 17:23:32 +0900

    Title: 재귀함수를 이용한 피보나치 수, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250421/재귀함수를 이용한 피보나치 수/fibonacci-using-recursive-function.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+
+int fib(int n) {
+    
+    if (n<=2)
+        return 1;
+    
+    return fib(n-1)+fib(n-2);
+}
+
+int main() {
+    cin >> N;
+
+    cout << fib(N);
+
+    return 0;
+}
